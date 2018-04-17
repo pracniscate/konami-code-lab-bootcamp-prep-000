9f377f355aef5fdc4540c1a1026deb30f409861b
Lorena <lorena.giddens@outlook.com>
2018-04-17 20:33:33 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,16 +3,22 @@ let index = 0;
 
 function init() {
   // add event listener
-  document.body.addEventListener('keydown', function(e)) {
+  document.body.addEventListener('keydown', function(e) {
     const key = parseInt(e.detail || e.which);
     
 // check if key value = code at the specified index; increase it if true
     if (key === code[index]) {
       index++;
       
-// if index reaches length of code, alert Egg Discoverer; then, reset
+// if index reaches length of code, alert Egg Discoverer; then, reset index
+      if (index === code.length) {
+        alert("You have found the hidden Egg!");
+        index = 0;
+      }
+
+// otherwise, reset index
+    } else {
+      index = 0;
     }
-    
-  }
-  
+  });
 }
