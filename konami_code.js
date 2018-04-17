f7b9717d40f73a305d14aa5b81ac57505c581316
Lorena <lorena.giddens@outlook.com>
2018-04-17 20:30:37 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,7 +3,15 @@ let index = 0;
 
 function init() {
   // add event listener
-  document.body.addEventListener('keydown', function(e) {
+  document.body.addEventListener('keydown', function(e)) {
+    const key = parseInt(e.detail || e.which);
+    
+// check if key value = code at the specified index; increase it if true
+    if (key === code[index]) {
+      index++;
+      
+// if index reaches length of code, alert Egg Discoverer; then, reset
+    }
     
   }
   
