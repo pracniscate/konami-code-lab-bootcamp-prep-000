065737e468f9a7077b6b5c71ba92c2ed6464b273
Lorena <lorena.giddens@outlook.com>
2018-04-17 20:26:45 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,7 +1,10 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
-
+let index = 0;
 
 function init() {
-  let index = 0;
+  // add event listener
+  document.body.addEventListener('keydown', function(e) {
+    
+  }
   
 }
